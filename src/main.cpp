6bb9bc759d6235032d150e88d8ca913957e1dd48
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2016-08-03 18:17:04 +0200

    Add OTA support.

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,81 @@
-#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoOTA.h>
 
-const char *ssid = "";
-const char *password = "";
 MDNSResponder mdns;
+
+const uint8_t pir_pin = 13;
+
+const char* ssid = "";
+const char* password = "";
 
 ESP8266WebServer server(80);
 
-const int led = 15;
-
 void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/html", "<html><head><title>Mega Title</title></head><body><p><b>Hello</b></p></body></html>");
-  digitalWrite(led, 0);
+  int read = analogRead(A0);
+  String response = "<html><head><title>Sensor</title></head><body><p><b>";
+  response += String(read);
+  response += "</b></p></body></html>";
+  server.send(200, "text/html", response);
 }
 
-void handleNotFound() {
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
-}
+void setup() {
+  pinMode(A0, INPUT);
 
-void setup(void) {
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
   Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+
   WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
 
-  if (mdns.begin("esp8266", WiFi.localIP())) {
+  if (mdns.begin("azesp", WiFi.localIP())) {
     Serial.println("MDNS responder started");
   }
 
-  server.on("/", handleRoot);
-
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
   });
 
-  server.onNotFound(handleNotFound);
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
 
   server.begin();
   Serial.println("HTTP server started");
 }
 
-void loop(void) {
+void loop() {
+  ArduinoOTA.handle();
   server.handleClient();
+
+  int read = analogRead(A0);
+  Serial.println(read);
+  delay(1000);
 }
